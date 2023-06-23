3b59fd839b7a4abcfd5bd4f4ef1e3bcbdda7e6fa
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-23 18:25:42 +0200

    ADD functions in js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
+const getStart = document.getElementById('start')
+const myContainer = document.querySelector('.numbers');
 
 
-const numberArray = [];
 
-addNumbers(5, 1, 5);
 
-console.log(numberArray);
 
-createElement();
+
+getStart.addEventListener('click', function(){
+    myContainer.innerHTML = '';
+    const numberArray = [];
+    addNumbers(5, 1, 5, numberArray);
+    createElement(numberArray);
+})
+
+
 
 
 
@@ -27,25 +34,24 @@ function getRandomNumber (min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
 
-function addNumbers(cycleNumber, min, max) {
+function addNumbers(cycleNumber, min, max, array) {
     let count = 0;
-    while (numberArray.length < cycleNumber) {
+    while (array.length < cycleNumber) {
         const oneNumber = getRandomNumber(min, max);
 
-        if(!numberArray.includes(oneNumber)) {
-            numberArray.push(oneNumber)
+        if(!array.includes(oneNumber)) {
+            array.push(oneNumber)
         }
 
         count++;
     }
 } 
 
-function createElement() {
-    const myContainer = document.querySelector('.numbers');
+function createElement(array) {
     for(i = 0; i < 5; i++) {
         const newDiv = document.createElement('div');
         myContainer.append(newDiv);
-        newDiv.append(numberArray[i]);
+        newDiv.append(array[i]);
         newDiv.classList.add('square')
     }
 
