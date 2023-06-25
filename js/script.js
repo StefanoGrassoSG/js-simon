fbfd661a64359843907eadd2b72a71e8cc1f3037
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-25 15:01:15 +0200

    ADD scores in js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
 const getStart = document.getElementById('start')
 const myContainer = document.querySelector('.numbers');
 
-
+const userNumbers = []
 
 
 
 
 getStart.addEventListener('click', function(){
+    score = 0;
     myContainer.innerHTML = '';
     const numberArray = [];
     addNumbers(5, 1, 5, numberArray);
     createElement(numberArray);
     addVisible();
+    setTimeout (
+        addInvisible, 3000,
+    );
+    setTimeout (
+        function() {
+            let info1 = prompt('insert numbers')
+            let info2 = prompt('insert numbers')
+            let info3 = prompt('insert numbers')
+            let info4 = prompt('insert numbers')
+            let info5 = prompt('insert numbers')
+            userNumbers.push(info1, info2, info3, info4, info5);
+            console.log(userNumbers)
+            for(let i = 0; i < numberArray.length; ++i){
+                if(numberArray[i] == info1 || numberArray[i] == info2 || numberArray[i] == info3 || numberArray[i] == info4 || numberArray[i] == info5)
+                 score++;
+               }
+            document.querySelector('.score-bar').append(score);
+        },
+        4000,
+       );
     
+    
+    
+   
 })
+
+
 
 
 
@@ -63,5 +89,12 @@ function addVisible() {
     const squares = document.querySelectorAll('.square')
     for(i = 0; i < squares.length; i++) {
         squares[i].classList.add('visible')
+    }
+}
+
+function addInvisible() {
+    const squares = document.querySelectorAll('.square')
+    for(i = 0; i < squares.length; i++) {
+        squares[i].classList.add('invisible')
     }
 }
